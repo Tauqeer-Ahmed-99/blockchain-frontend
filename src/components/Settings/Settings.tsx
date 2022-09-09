76504da9857a3b526df80966d57393581444f4ae
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2022-09-09 17:11:15 +0530

    commit

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 
 import Switch from "../Switch/Switch";
 import SunIcon from "../../assets/icons/sun.svg";
